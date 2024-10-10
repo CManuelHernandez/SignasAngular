e2906b105253d3871f2688560c2d46491cd30609
C.Manuel Hernández <cmanuelhm87@gmail.com>
2024-10-10 12:41:53 +0200

    Estructura de la aplicación

---
+++ src/app/products/pages/product-page/product-page.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  templateUrl: './product-page.component.html',
+  styleUrl: './product-page.component.css',
+})
+export class ProductPageComponent {}
