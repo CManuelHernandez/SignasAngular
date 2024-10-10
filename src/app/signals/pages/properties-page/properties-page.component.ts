76cfbcf3a7291f5ff81a6af467460c2956fa5ed7
C.Manuel Hernández <cmanuelhm87@gmail.com>
2024-10-10 17:51:46 +0200

    Estructura de pantalla

--- src/app/signals/pages/properties-page/properties-page.component.ts
+++ src/app/signals/pages/properties-page/properties-page.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-properties-page',
   templateUrl: './properties-page.component.html',
-  styleUrl: './properties-page.component.css'
+  styleUrl: './properties-page.component.css',
 })
 export class PropertiesPageComponent {
-
+  onFieldUpdated(field: string, value: string) {
+    console.log({ field, value });
+  }
 }
